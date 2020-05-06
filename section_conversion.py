f9bbd01a1192b5d80876a3ca2eb2c8fdc5667c43
jonajames <serv37@live.it>
2020-05-06 23:55:53 +0200

    flip X axis

--- section_conversion.py
+++ section_conversion.py
@@ -1,25 +1,22 @@
-def convertSection(section,convert_block_function):
+from block_conversion import convertBlock
+
+def coord(z,y,x):
+    return (z<<8) | (y<<4) | (15-x) 
+
+def convertSection(section):
     converted_section = {
-        'pos' : 0,
+        'pos' : (0,0,0),
         'param0' : [0]*4096,
-        'param1' : [15]*4096,
+        'param1' : [255]*4096,
         'param2' : [0]*4096,
         'mappings' : [],
     }
     
     # Convert block coordinates
-    def getBlockAsInteger(x,y,z):
-        u = z*16777216 + y*4096 + x
-        while u >= 2**63: u -= 2**64
-        while u <= -2**63: u += 2**64
-        return u
-    chunk_pos = section["mca"].split(".")
-    assert(chunk_pos[3]=="mca")
-    converted_section["pos"] = getBlockAsInteger(
-        section['x'] + int(chunk_pos[1])*32,
-        section['y'] - 10,
-        section['z'] + int(chunk_pos[2])*32,
-    )
+    r,chunk_x,chunk_z,mca = section["mca"].split(".")
+    chunk_x = (-int(chunk_x) << 5) | (31-section['x']) # X axis is swapped
+    chunk_z = (int(chunk_z) << 5) | section['z']
+    converted_section["pos"] = (chunk_x,section['y'],chunk_z)
 
     # Skip if empty
     if section["blocks"].is_empty():
@@ -30,16 +27,14 @@ def convertSection(section,convert_block_function):
     for y in range(16):
         for z in range(16):
             for x in range(16):
-                try:
-                    itemstring,param1,param2 = convert_block_function(section["blocks"][y*256+z*16+x])
-                except Exception as e:
-                    print("Conversion of block",section["blocks"][y*256+z*16+x],"failed with",e)
+                # Convert block
+                itemstring,param1,param2 = convertBlock(section["blocks"][coord(y,z,x)])
                 if itemstring not in converted_section["mappings"]:
                     converted_section["mappings"].append(itemstring)
                 # Coordinates are swapped from XZY to XYZ
-                converted_section["param0"][z*256+y*16+x] = converted_section["mappings"].index(itemstring)
-                converted_section["param1"][z*256+y*16+x] = param1
-                converted_section["param2"][z*256+y*16+x] = param2
+                converted_section["param0"][coord(z,y,x)] = converted_section["mappings"].index(itemstring)
+                converted_section["param1"][coord(z,y,x)] = param1
+                converted_section["param2"][coord(z,y,x)] = param2
 
     # End
     return converted_section
